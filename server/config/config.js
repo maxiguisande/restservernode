172ec0ce2e464cc8aaee4bd5b32ed4274f0e96dc
Maximiliano Guisande <maxipg@gmail.com>
2020-02-02 18:20:17 -0300

    config

--- server/config/config.js
+++ server/config/config.js
@@ -20,6 +20,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://MAXIGUISI:ftZDLr9l1TvbqSQd@cluster0-ih4z3.mongodb.net/cafe';
+    urlDB = process.env.mongoDB;
 };
 process.env.URLDB = urlDB;
